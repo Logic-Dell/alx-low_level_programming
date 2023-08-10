d4007962dbce3e2b9582d67ef842bdddf40fa71e
Logic-Dell <inteldell296@gmail.com>
2023-08-10 12:50:48 -0700

    First c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is a function
+ * return - returns the function main
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
